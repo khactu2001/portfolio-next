3b5ad77e878537c1c7b618c10acdcccda76e8796
LeeM2 <tulk@brickmate.vn>
2024-05-27 22:32:34 +0700

    - install bootstrap - create a simple layout using bootstrap

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+import Button from "react-bootstrap/Button";
+export default function Page() {
+  return (
+    <div>
+      <Button variant="success">About</Button>
+      <h1>Hello, Next.js!</h1>
+    </div>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      {/* <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+        crossorigin="anonymous"
+      /> */}
       <body className={inter.className}>{children}</body>
     </html>
   );
