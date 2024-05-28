6f3bd6860db092af15c90b5bc33e328af226b68d
LeeM2 <tulk@brickmate.vn>
2024-05-28 16:18:49 +0700

    import swr, render data response from api

---
+++ app/components/home/home.footer.tsx
@@ -0,0 +1,8 @@
+"use client";
+export function Footer() {
+  return (
+    <div className="flex justify-content-center bg-gray-300">
+      <span>Footer</span>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 import { Col, Row } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
 import Table from "react-bootstrap/Table";
+import useSWR from "swr";
+import { User } from "./type";
+import { Footer } from "./components/home/home.footer";
+import NavBarHeader from "./components/home/home.header";
 
-function TableContent() {
+function TableContent({ data }: { data: User[] }) {
   return (
     <Table striped bordered hover>
       <thead>
@@ -18,69 +19,40 @@ function TableContent() {
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
+        {/* <tr>
           <td>3</td>
           <td colSpan={2}>Larry the Bird</td>
           <td>@twitter</td>
-        </tr>
+        </tr> */}
+
+        {data.map((user) => (
+          <tr key={user.id}>
+            <td>{user.id}</td>
+            <td>{user.title}</td>
+            <td>{user.body}</td>
+            <td>{user.userId}</td>
+          </tr>
+        ))}
       </tbody>
     </Table>
   );
 }
-function NavBarHeader() {
-  return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
-}
-
-function Footer() {
-  return (
-    <div className="flex justify-content-center bg-gray-300">
-      <span>Footer</span>
-    </div>
-  );
-}
 
 function Home() {
+  const fetcher = (url: string) => fetch(url).then((res) => res.json());
+  const { data, error, isLoading } = useSWR(
+    "https://jsonplaceholder.typicode.com/posts",
+    fetcher
+  );
+
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+  console.log("data", data);
   return (
     <div>
       <NavBarHeader />
       <Container fluid className="flex justify-content-center px-md-10">
-        <TableContent />
+        <TableContent data={data} />
       </Container>
       <Footer />
     </div>
